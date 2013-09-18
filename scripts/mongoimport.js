de2769fce9018a2e479da7be4fedcde14da27ced
Roy Evan Sia <roysia2004@gmail.com>
2013-09-18 22:16:37 +0800

    added more cities in america and modified mongoimport

--- scripts/mongoimport.js
+++ scripts/mongoimport.js
@@ -11,26 +11,36 @@ var data = require('./data'),
     Region = mongoose.model('Region'),
     City = mongoose.model('City');
 
-City.remove({}).exec();
-Region.remove({}).exec();
+City.remove({}, function(err) {
+  if (err) {
+    console.log('Error removing existing cities!');
+    return;
+  }
 
-data.forEach(function(data) {
-  Region.create({ name: data.region }, function(err, region) {
+  Region.remove({}, function(err) {
     if (err) {
-      console.log('Error populating regions!');
+      console.log('Error removing existing regions!');
       return;
     }
-    console.log('Region: ' + region.name + ' created.');
 
-    data.cities.forEach(function(city) {
-      var _city = new City(city);
-      _city.region = region;
-      _city.save(function(err, city) {
-        if (err) console.log('Error populating cities!');
-        else console.log('City: ' + city.name + ' created.');
+    data.forEach(function(data) {
+      Region.create({ name: data.region }, function(err, region) {
+        if (err) {
+          console.log('Error populating regions!');
+          return;
+        }
+
+        console.log('Region: ' + region.name + ' created.');
+        data.cities.forEach(function(city) {
+          var _city = new City(city);
+          _city.region = region;
+
+          _city.save(function(err, city) {
+            if (err) console.log('Error populating cities!');
+            else console.log('City: ' + city.name + ' created.');
+          });
+        });
       });
     });
   });
-});
-
-console.log('Data import finished.');
+})

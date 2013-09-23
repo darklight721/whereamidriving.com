75c73a8a033779175028e17b98cd10ca94b661d2
Roy Sia <roy@caresharing.ph>
2013-09-23 14:04:16 +0800

    added stats frontend (partial)

--- public/scripts/controllers/stats.js
+++ public/scripts/controllers/stats.js
@@ -3,8 +3,30 @@
 angular.module('GuessApp')
   .controller('StatsCtrl', function ($scope, Engine) {
 
-    $scope.init = function() {
+    $scope.regions = Engine.regions();
+    $scope.stats = Engine.stats();
 
+    var statsPerRegion = {};
+
+    $scope.statsForRegion = function(regionId) {
+      if (!statsPerRegion[regionId]) {
+        statsPerRegion[regionId] = findWhere($scope.stats.cities, { region: regionId });
+      }
+
+      return statsPerRegion[regionId];
     };
+
+    function findWhere = function(list, props) {
+      for (var i = 0, length = list.length; i < length; ++i) {
+        var found = false;
+        for (var k in props) {
+          if (props.hasOwnProperty(k)) {
+            found = list[i][k] == props[k];
+          }
+        }
+        found && return list[i];
+      }
+      return null;
+    }
 
   });
